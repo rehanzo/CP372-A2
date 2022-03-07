7ded2390db70df205590f8a120a506d93680d7e2
Rehan Rana <rehanalirana@tuta.io>
2022-03-07 16:45:10 -0500

    completes reciever corruption check func

--- receiver.py
+++ receiver.py
@@ -5,7 +5,7 @@ class receiver:
         ''' Checks if a received packet has been corrupted during transmission.
         Return true if computed checksum is different than packet checksum.'''
 
-        return
+        return packet.checksum != checksumCalc(packet.payload)
    
     def isDuplicate(self, packet):
         '''checks if packet sequence number is the same as expected sequence number'''
